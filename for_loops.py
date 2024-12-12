2b6655facb49ff758d5df0d2aa60a0c81c127dad
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-12-11 16:01:35 -0800

    Add new examples of reverse for loops

--- for_loops.py
+++ for_loops.py
@@ -31,3 +31,21 @@ fruits = ["apple", "banana", "cherry"]
 for x in fruits:
     print(x)
 # Output: apple banana cherry
+
+# Three ways to iterate in reverse in Python
+
+# 1. Using the reversed() function:
+# This is the most straightforward and readable way to iterate in reverse:
+my_list = [1, 2, 3, 4, 5]
+for item in reversed(my_list):
+    print(item)
+
+# 2. Using the range() function with a negative step:
+# This method is useful if you need more control over the iteration, such as stepping by more than one element:
+for i in range(len(my_list) - 1, -1, -1):
+    print(my_list[i])
+
+# 3. Using slicing with a negative step:
+# This is a concise way to reverse a sequence, but it creates a new reversed copy:
+for item in my_list[::-1]:
+    print(item)
